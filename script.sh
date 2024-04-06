ba8e572200a86cfcc4b5f6cc619ce8fe781a127b
veeresh-devops <98221726+veeresh-devops@users.noreply.github.com>
2024-04-06 14:58:12 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 if [ "$BUILD_STATUS" = "SUCCESS" ]; then
   echo "Build was successful, running additional steps..."
   # Add additional commands here
-  touch f1
+  touch freestyle1
 else
   echo "Build failed, skipping additional steps."
 fi
